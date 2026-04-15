ce742c72c6b06b81ff96405e83776f77ac754bfc
ItsJuniorDias <reabilitado97@gmail.com>
2026-04-15 20:54:00 -0300

    chore: bun script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -8,13 +8,17 @@ cd $CI_PRIMARY_REPOSITORY_PATH
 
 echo "➡️ Instalando o Node.js..."
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-brew install node
+brew install node || brew link --overwrite node
 
-echo "➡️ Instalando as dependências do projeto..."
+echo "➡️ Instalando o Bun..."
+# Como o Node acabou de ser instalado, usamos o npm para instalar o Bun globalmente
+npm install -g bun
+
+echo "➡️ Instalando as dependências do projeto com Bun..."
 bun install
 
 echo "➡️ Instalando os Pods do iOS..."
 cd ios
-pod install
+pod install --repo-update
 
-echo "✅ Configuração concluída com sucesso!"
+echo "✅ Script pós-clone finalizado com sucesso!"
